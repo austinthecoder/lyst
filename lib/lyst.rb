85083d907d9db103df274f2079f3423540b2313b
Austin Schneider <me@austinschneider.com>
2020-10-26 18:21:59 -0400

    bundle gem lyst

---
+++ lib/lyst.rb
@@ -0,0 +1,6 @@
+require "lyst/version"
+
+module Lyst
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/lyst_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Lyst do
+  it "has a version number" do
+    expect(Lyst::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
