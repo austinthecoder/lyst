a1e0d5900c285aee7cba44fa76a475fee29a2d80
Austin Schneider <me@austinschneider.com>
2020-10-26 18:41:09 -0400

    clean up

--- lib/lyst.rb
+++ lib/lyst.rb
@@ -1,6 +1,5 @@
 require "lyst/version"
 
 module Lyst
-  class Error < StandardError; end
-  # Your code goes here...
+
 end
--- spec/lyst_spec.rb
+++ spec/lyst_spec.rb
@@ -1,9 +1,2 @@
 RSpec.describe Lyst do
-  it "has a version number" do
-    expect(Lyst::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
